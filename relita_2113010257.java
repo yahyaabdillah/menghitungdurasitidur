a6499bcf2340a2c85b95fef6a862e3e8944c212a
RelitaaK <133621488+RelitaaK@users.noreply.github.com>
2023-08-20 11:40:58 +0700

    Add files via upload

---
+++ relita_2113010257.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RelitaKurnia
+ */
+public class relita_2113010257 {
+    public static void main(String[] args) {
+        System.out.println("relita_2113010257_4f");
+    }
+}
